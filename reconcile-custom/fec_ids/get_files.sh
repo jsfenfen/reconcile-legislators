3d7213ddfff01979cf292d47c2fb9a15128523ab
Jacob Fenton <jfenton@sunlightfoundation.com>
2013-01-24 15:21:31 -0500

    add data back to 1980

--- reconcile-custom/fec_ids/get_files.sh
+++ reconcile-custom/fec_ids/get_files.sh
@@ -1,9 +1,7 @@
-# grab the files we need. Earlier years are available, tho
-# eventually the format changes
+# grab candidate files. Not sure about '78.
 datadir=./data
 
-for year in '88' '90' '92' '94' '96' '98' 
-# for year in '00' '02' '04' '06' '08' '10' '12' 
+for year in '80' '82' '84' '86' '88' '90' '92' '94' '96' '98' 
 do
     echo "Getting files for: $year"
     
@@ -12,6 +10,18 @@ do
     curl -o $datadir/$year/cn$year.zip ftp://ftp.fec.gov/FEC/19$year/cn$year.zip 
     # Y2K:
     # curl -o $datadir/$year/cn$year.zip ftp://ftp.fec.gov/FEC/20$year/cn$year.zip 
+    unzip -o $datadir/$year/cn$year.zip -d $datadir/$year
+
+    sleep 1 
+
+done
+
+for year in '00' '02' '04' '06' '08' '10' '12' 
+do
+    echo "Getting files for: $year"
+    
+    mkdir -p $datadir/$year/
+    curl -o $datadir/$year/cn$year.zip ftp://ftp.fec.gov/FEC/20$year/cn$year.zip 
     unzip -o $datadir/$year/cn$year.zip -d $datadir/$year
 
     sleep 1 
